b771f162d831dab657e11765142db28feda37f44
jcowdells <jcowdells@gmail.com>
2025-04-07 00:43:32 +0100

    added some bogus opengl code so it looks like i havent wasted so much time on a yml parser

--- main.c
+++ main.c
@@ -17,17 +17,24 @@
 #include "core/yml.h"
 #include "tekgl/material.h"
 
+#include "tekgl/camera.h"
+
+#include <time.h>
+#include <math.h>
+
 #define printException(x) tekLog(x)
 
 float width, height;
+mat4 perp_projection;
 
 void tekMainFramebufferCallback(const int fb_width, const int fb_height) {
     width = (float)fb_width;
     height = (float)fb_height;
+    glm_perspective(1.2f, width / height, 0.1f, 100.0f, perp_projection);
 }
 
 int render() {
-    printException(tekInit("TekPhysics", 640, 480));
+    tekChainThrow(tekInit("TekPhysics", 640, 480));
     width = 640.0f;
     height = 480.0f;
 
@@ -36,74 +43,151 @@ int render() {
     TekBitmapFont verdana;
     printException(tekCreateFreeType());
 
-    printException(tekCreateBitmapFont("../res/verdana.ttf", 0, 64, &verdana));
+    tekChainThrow(tekCreateBitmapFont("../res/verdana.ttf", 0, 64, &verdana));
 
     TekText text;
-    printException(tekCreateText("TekPhysics Alpha v1.0", 20, &verdana, &text));
+    tekChainThrow(tekCreateText("TekPhysics Alpha v1.0", 20, &verdana, &text));
 
     tekDeleteFreeType();
+
+    glEnable(GL_DEPTH_TEST);
+    glDepthFunc(GL_LESS);
 
     glEnable(GL_BLEND);
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
-    vec2 point_a = {0.0f, 0.0f};
-    vec2 point_b = {640.0f, 480.0f};
-    vec4 line_color = {1.f, 1.f, 1.f, 1.f};
+    TekMesh walls = {};
 
-    TekGuiLine line;
+    const float wall_vertices[] = {
+        0.0f, 6.0f, -3.0f, 1.0f, 0.0f, 0.0f,
+        0.0f, 6.0f,  3.0f, 1.0f, 0.0f, 0.0f,
+        0.0f, 0.0f,  3.0f, 1.0f, 0.0f, 0.0f,
+        0.0f, 0.0f, -3.0f, 1.0f, 0.0f, 0.0f,
+        0.0f, 0.0f, -3.0f, 0.0f, 1.0f, 0.0f,
+        0.0f, 0.0f,  3.0f, 0.0f, 1.0f, 0.0f,
+        12.0f, 0.0f,  3.0f, 0.0f, 1.0f, 0.0f,
+        12.0f, 0.0f, -3.0f, 0.0f, 1.0f, 0.0f
+    };
 
-    printException(tekCreateLine(point_a, point_b, 1.f, line_color, &line));
+    const uint wall_indices[] = {
+        0, 1, 2,
+        0, 2, 3,
+        4, 5, 6,
+        4, 6, 7
+    };
 
-    TekGuiOval oval;
+    const uint wall_layout[] = {
+        3, 3
+    };
 
-    printException(tekCreateOval(point_a, point_b, 10.0f, 0, line_color, &oval));
+    tekChainThrow(tekCreateMesh(wall_vertices, 48, wall_indices, 12, wall_layout, 2, &walls));
+
+    TekMesh cube = {};
+
+    const float cube_vertices[] = {
+        -1.0f,  1.0f, -1.0f,  0.0f, 1.0f, 0.0f,
+        -1.0f,  1.0f,  1.0f,  0.0f, 1.0f, 0.0f,
+         1.0f,  1.0f,  1.0f,  0.0f, 1.0f, 0.0f,
+         1.0f,  1.0f, -1.0f,  0.0f, 1.0f, 0.0f,
+         1.0f,  1.0f,  1.0f,  1.0f, 0.0f, 0.0f,
+         1.0f,  1.0f, -1.0f,  1.0f, 0.0f, 0.0f,
+         1.0f, -1.0f, -1.0f,  1.0f, 0.0f, 0.0f,
+         1.0f, -1.0f,  1.0f,  1.0f, 0.0f, 0.0f,
+        -1.0f,  1.0f,  1.0f,  0.0f, 0.0f, 1.0f,
+         1.0f,  1.0f,  1.0f,  0.0f, 0.0f, 1.0f,
+         1.0f, -1.0f,  1.0f,  0.0f, 0.0f, 1.0f,
+        -1.0f, -1.0f,  1.0f,  0.0f, 0.0f, 1.0f
+    };
+
+    const uint cube_indices[] = {
+        0, 1, 2,
+        0, 2, 3,
+        4, 5, 6,
+        4, 6, 7,
+        8, 9, 10,
+        8, 10, 11
+    };
+
+    const uint cube_layout[] = {
+        3, 3
+    };
+
+    tekCreateMesh(cube_vertices, 72, cube_indices, 36, cube_layout, 2, &cube);
+
+    vec3 camera_position = {5.0f, 3.0f, 5.0f};
+    vec3 light_color = {0.4f, 0.4f, 0.4f};
+    vec3 light_position = {4.0f, 12.0f, 0.0f};
+
+    uint shader_program;
+    tekChainThrow(tekCreateShaderProgramVF("../shader/vertex.glvs", "../shader/fragment.glfs", &shader_program));
+
+    tekBindShaderProgram(shader_program);
+
+    tekShaderUniformVec3(shader_program, "light_color", light_color);
+    tekShaderUniformVec3(shader_program, "light_position", light_position);
+    tekShaderUniformVec3(shader_program, "camera_pos", camera_position);
+
+    mat4 wall_model;
+    mat4 view;
+
+    glm_mat4_identity(wall_model);
+
+    vec3 center = {0.0f, 0.0f, 0.0f};
+    vec3 up = {0.0f, 1.0f, 0.0f};
+    glm_lookat(camera_position, center, up, view);
+
+    glm_perspective(1.2f, width / height, 0.1f, 100.0f, perp_projection);
+
+    tekShaderUniformMat4(shader_program, "view", view);
+
+    vec3 cube_position = {3.0f, 1.0f, 0.0f};
+    mat4 cube_model;
+
+    struct timespec t;
+    clock_gettime(CLOCK_REALTIME, &t);
+    long time_ns = t.tv_sec * 1000000000 + t.tv_nsec;
+    double delta = 0.0;
+    double multiplier = -1.0;
 
     while (tekRunning()) {
-        glClearColor(0.2f, 0.2f, 0.21f, 1.0f);
+        clock_gettime(CLOCK_REALTIME, &t);
+        long delta_ns = (t.tv_sec * 1000000000 + t.tv_nsec) - time_ns;
+        delta = ((double)delta_ns) / 1000000000.0;
+        time_ns = t.tv_sec * 1000000000 + t.tv_nsec;
 
-        printException(tekDrawLine(&line));
-        printException(tekDrawOval(&oval));
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+
+
+        tekBindShaderProgram(shader_program);
+        tekShaderUniformMat4(shader_program, "projection", perp_projection);
+
+        tekShaderUniformMat4(shader_program, "model", wall_model);
+        tekDrawMesh(&walls);
+
+        cube_position[0] += delta * multiplier;
+        if (cube_position[0] < 1.0f) {
+            cube_position[0] = 1.0f;
+            multiplier = 1.0;
+        }
+
+        glm_translate_make(cube_model, cube_position);
+        tekShaderUniformMat4(shader_program, "model", cube_model);
+        tekDrawMesh(&cube);
 
         printException(tekDrawText(&text, width - 185.0f, 5.0f));
         tekUpdate();
     }
 
+    tekDeleteShaderProgram(shader_program);
+    tekDeleteMesh(&walls);
     tekDeleteTextEngine();
     tekDeleteText(&text);
     tekDelete();
     return SUCCESS;
 }
 
-void testYml() {
-    tekLog(tekInit("TekPhysics", 640, 480));
-
-    float vertices = {
-        -0.5f, -0.5f,
-         0.5f, -0.5f,
-         0.5f,  0.5f,
-        -0.5f,  0.5f
-    }
-
-    uint indices = {
-        0, 1, 2,
-        0, 2, 3
-    }
-
-    uint layout = {
-        2
-    }
-
-    TekMaterial material = {};
-    tekLog(tekCreateMaterial("../res/material.yml", &material));
-    printf("material: %d\n", material.uniforms[1]->type);
-    tekLog(tekBindMaterial(&material));
-}
-
 int main(void) {
     tekInitExceptions();
-    // if (render()) {
-    //     tekPrintException();
-    // }
-    testYml();
+    tekLog(render());
     tekCloseExceptions();
 }
--- tekgl/manager.c
+++ tekgl/manager.c
@@ -103,7 +103,7 @@ exception tekUpdate() {
     glfwPollEvents();
 
     // clear framebuffer for next draw call
-    glClear(GL_COLOR_BUFFER_BIT);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
     return SUCCESS;
 }
